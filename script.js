f280181024f39ae40dff9562dc2890765b714138
ftm-khamse <khamsehfatemeh347@gmail.com>
2025-08-30 03:32:14 -0700

    design and style shopping cart

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const bar = document.getElementById('bar-btn');
 const navbar = document.querySelector('.nav');
+const shopBtn = document.getElementById('shop-btn');
+const shopCart = document.querySelector('.shopping-cart');
+const item = navbar.querySelectorAll('a');
 
 bar.addEventListener('click',() => {
-    const item = navbar.querySelectorAll('a');
-    item.forEach((item ) => item.classList.toggle('active'));
     
+    item.forEach((item ) => item.classList.toggle('active'));
+    shopCart.classList.remove('active');
 });
+
+shopBtn.addEventListener('click',() => {
+    shopCart.classList.toggle('active');
+    item.forEach((item ) => item.classList.remove('active'));
+})
