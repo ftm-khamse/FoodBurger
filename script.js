19234246ad1517c1cbe95550a397ed886aec2347
ftm-khamse <khamsehfatemeh347@gmail.com>
2025-08-30 04:11:21 -0700

    design and style search bar

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ const navbar = document.querySelector('.nav');
 const shopBtn = document.getElementById('shop-btn');
 const shopCart = document.querySelector('.shopping-cart');
 const item = navbar.querySelectorAll('a');
+const searchBtn = document.getElementById('search-btn');
+const search = document.querySelector('.search');
 
 bar.addEventListener('click',() => {
     
     item.forEach((item ) => item.classList.toggle('active'));
     shopCart.classList.remove('active');
+    search.classList.remove('active');
 });
 
 shopBtn.addEventListener('click',() => {
     shopCart.classList.toggle('active');
     item.forEach((item ) => item.classList.remove('active'));
-})
+     search.classList.remove('active');
+});
+
+searchBtn.addEventListener('click',() => {
+    search.classList.toggle('active');
+    shopCart.classList.remove('active');
+    item.forEach((item ) => item.classList.remove('active'));
+});
+
+window.onscroll = () => {
+    search.classList.remove('active');
+    shopCart.classList.remove('active');
+    item.forEach((item ) => item.classList.remove('active'));
+}
